c7f05070b05122ddd060e7c7964d2f5bf201eb92
Jonathan Barbeau <Barbeau.jonathan@gmail.com>
2017-03-24 21:44:55 -0400

    des petits tests la

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,8 +1,26 @@
 var game = {};
+var logo;
 
 game.create = function () {
-  var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+
+  logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
   logo.anchor.setTo(0.5, 0.5);
+
+  game.stage.backgroundColor = '#0072bc';
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.enableBody(logo);
+
+
+  logo.body.collideWorldBounds = true;
+
+  logo.scale.setTo(0.5);
+  logo.body.allowRotation = false;
 };
+
+game.update = function () {
+  //logo.position.x += 0.9;
+  logo.rotation = game.physics.arcade.moveToPointer(logo, 60, game.input.activePointer, 500);
+}
 
 module.exports = game;
