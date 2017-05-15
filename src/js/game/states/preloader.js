a6d6568ac5cf2895736a786723cf2a791297a549
Unknown <jobarbo@me.com>
2017-05-15 15:22:30 -0400

    Removed test level and added level1

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -27,7 +27,7 @@ preloader.preload = function () {
   this.game.load.spritesheet('mute','images/muteButton.png',28,22);
 
   //preload tilemap
-  this.game.load.tilemap('niveau1', 'images/niveautest.json', null, Phaser.Tilemap.TILED_JSON);
+  this.game.load.tilemap('niveau1', 'images/niveau1.json', null, Phaser.Tilemap.TILED_JSON);
 
   //  Next we load the tileset. This is just an image, loaded in via the normal way we load images:
   this.game.load.image('pixel','images/pixel.png');
