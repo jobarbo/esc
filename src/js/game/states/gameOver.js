20d17f43d2f5f56cde593de008b77ff95701c30f
Jonathan Barbeau <Barbeau.jonathan@gmail.com>
2017-05-19 15:41:24 -0400

    Added gameOver screen

--- src/js/game/states/gameOver.js
+++ src/js/game/states/gameOver.js
@@ -1,6 +1,6 @@
-var mainMenu = {};
+var gameOver = {};
 
-mainMenu.create = function () {
+gameOver.create = function () {
     //this.game.add.image(0, 0, 'color_bg');
     //city = this.game.add.image(0, 0, 'city_bg');
     //city.scale.setTo(1.5,1.5);
@@ -12,7 +12,7 @@ mainMenu.create = function () {
 
     //affiche le nom du jeu
     var nameLabel = this.game.add.text(this.game.width / 2, -50, 'Game Over', {
-        font: '50px pixelSmall',
+        font: '24px pixelSmall',
         fill: '#ffffff',
         align: 'center'
     });
@@ -21,38 +21,45 @@ mainMenu.create = function () {
         y: 80
     }, 1000).easing(Phaser.Easing.Bounce.Out).start();
 
-    var text;
-    if (this.game.device.desktop) {
-        text = 'Peser sur ESPACE\n pour recommencer';
-    } else {
-        text = "Toucher l'écran\n pour recommencer";
-    }
-
-    //text explicatif sur comment debuter le jeu
-    var startLabel = this.game.add.text(this.game.width / 2, this.game.height - 80, text, {
-        font: '15px pixelVector',
+    returnMenuLabel = this.game.add.text(this.game.width / 2, this.game.height - 110, 'Retourner au menu principal', {
+        font: '10px smallest',
         fill: '#ffffff',
         align: 'center'
     });
-    startLabel.anchor.setTo(0.5, 0.5);
+    returnMenuLabel.anchor.setTo(0.5, 0.5);
+    returnMenuLabel.inputEnabled = true;
+    returnMenuLabel.events.onInputOver.add(this.mouseOver, returnMenuLabel);
+    returnMenuLabel.events.onInputOut.add(this.mouseOut, returnMenuLabel);
+    returnMenuLabel.events.onInputDown.add(this.restartGame, this);
+
 
     //ajout du bouton qui fait appel a la fonction toggleSound
     this.muteButton = this.game.add.button(20, 20, 'mute', this.toggleSound, this);
 
     //si le jeu est deja sans son, afficher le speaker mute
     this.muteButton.frame = this.game.sound.mute ? 1 : 0;
-
-    //cree une nouvelle variable Phaser keyboard : la fleche haut
-    //lorsque pesee elle apelle Start
-    var upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    upKey.onDown.add(this.demarrer, this);
+    
 
     if (!this.game.device.desktop) {
-        this.game.input.onDown.add(this.demarrer, this);
+        this.game.input.onDown.add(this.restartGame, this);
     }
 };
+gameOver.restartGame = function () {
+    this.game.state.start('mainMenu');
+}
 
-mainMenu.toggleSound = function () {
+gameOver.mouseOver = function (textLabel) {
+    textLabel.fill = "#ffff44";
+    this.game.canvas.style.cursor = 'pointer';
+}
+
+
+gameOver.mouseOut = function (textLabel) {
+    textLabel.fill = "#ffffff";
+    this.game.canvas.style.cursor = 'inherit';
+}
+
+gameOver.toggleSound = function () {
     //change la variable de vrai a faux ou vice versa
     //quand 'game.sound.mute = true', Phaser va muter le jeu
     this.game.sound.mute = !this.game.sound.mute;
@@ -61,20 +68,5 @@ mainMenu.toggleSound = function () {
     this.muteButton.frame = this.game.sound.mute ? 1 : 0;
 }
 
-mainMenu.demarrer = function () {
-    //this.game.state.start('level1');
 
-    //Si lon tappe sur le coin haut gauche sur mobile
-    if (!this.game.device.desktop && this.game.input.y < 50 && this.game.input.x < 60) {
-        // cela veux dire que l'usager veux muter le son et pas demarrer le jeu
-        return;
-
-    }
-    // demarre le jeu
-    //arrete la musique
-    this.music.stop();
-    this.game.state.start('level1');
-    
-};
-
-module.exports = mainMenu;
+module.exports = gameOver;
